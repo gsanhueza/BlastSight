9d09e658cbb008c1e1c1935fe3f209beadcd9dd5
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-03-22 12:29:09 -0300

    Parse DXF updated

--- Model/dxfparser.py
+++ Model/dxfparser.py
@@ -7,40 +7,51 @@ from collections import OrderedDict
 class DXFParser:
     def __init__(self, filepath):
         self.dxf = dxfgrabber.readfile(filepath)
+        self.vertices = ()
+        self.faces = ()
 
+    # Returns the entities of the DXF file
     def get_entities(self):
         return self.dxf.entities
+
+    # Read the DXF file and create tuples of vertices and faces
+    def _parse_entities(self):
+        vertices_dict = OrderedDict()
+        # Detect vertices and faces
+        index = 0
+        faces = []
+        for entity in self.get_entities():
+            face_pointers = []
+            # Vertices
+            for vertex in entity.points:
+                if vertex not in vertices_dict:
+                    vertices_dict[vertex] = index
+                    index += 1
+
+                # Faces
+                face_pointers.append(vertices_dict[vertex])
+            faces.append(tuple(face_pointers))
+
+        self.vertices = tuple(vertices_dict.keys())
+        self.faces = faces
+
+    # Create OFF file
+    # WARNING At the moment we're creating a 4-vertices OFF file
+    def print_off(self):
+        self._parse_entities()
+
+        print('OFF')
+        V = len(self.vertices)
+        F = len(self.faces)
+        E = V + F - 2
+        print(f'{V} {F} {E}')
+        for v in self.vertices:
+            print(f'{v[0] - 73736.554} {v[1] - 57510.047} {v[2] - 362.913}')
+
+        for f in self.faces:
+            print(f'4 {f[0]} {f[1]} {f[2]} {f[3]}')
 
 
 if __name__ == '__main__':
     parser = DXFParser('caseron.dxf')
-
-    vertices_dict = OrderedDict()
-    # Detect vertices
-    index = 0
-    for entity in parser.get_entities():
-        for vertex in entity.points:
-            if vertex not in vertices_dict:
-                vertices_dict[vertex] = index
-                index += 1
-
-    # Detect faces
-    faces = []
-    for entity in parser.get_entities():
-        face_pointers = []
-        for vertex in entity.points:
-            face_pointers.append(vertices_dict[vertex])
-        faces.append(tuple(face_pointers))
-
-    # Create OFF file
-    # WARNING At the moment we're creating a 4-vertices OFF file
-    print('OFF')
-    V = len(vertices_dict)
-    F = len(faces)
-    E = V + F - 2
-    print(f'{V} {F} {E}')
-    for v in vertices_dict.keys():
-        print(f'{v[0] - 73736.554} {v[1] - 57510.047} {v[2] - 362.913}')
-
-    for f in faces:
-        print(f'4 {f[0]} {f[1]} {f[2]} {f[3]}')
+    parser.print_off()
