f1964697c95d4fcaa68075b24903c3729e93166f
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-03-28 16:52:29 -0300

    Simplification in opengl function names

--- View/openglwidget.py
+++ View/openglwidget.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-from OpenGL import GL, GLUT
+from OpenGL.GL import *
 from PySide2.QtWidgets import QOpenGLWidget
 from PySide2.QtGui import *
 from PySide2.QtCore import Qt, Slot
@@ -62,18 +62,18 @@ class OpenGLWidget(QOpenGLWidget):
         print(self.model.get_vertices())
         vertices = np.array([0.0, 1.0, -1.0, -1.0, 1.0, -1.0], dtype=np.float32)
 
-        bufferId = GL.glGenBuffers(1)
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, bufferId)
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL.GL_STATIC_DRAW)
+        bufferId = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, bufferId)
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
 
-        GL.glEnableVertexAttribArray(0)
-        GL.glVertexAttribPointer(0, 2, GL.GL_FLOAT, GL.GL_FALSE, 0, None)
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, None)
 
     def paintGL(self):
         painter = QPainter(self)
 
-        GL.glRotated(self.rotation, 0.0, 1.0, 0.0)
-        GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
+        glRotated(self.rotation, 0.0, 1.0, 0.0)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
 
     def resizeGL(self, width, height):
         super().resizeGL(width, height)
