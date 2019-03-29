9c05a1d6c2b60ae50ed075a7c2da386205b36427
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-03-29 17:44:01 -0300

    WIP OpenGL port, errors in glVertexAttribPointer

--- View/openglwidget.py
+++ View/openglwidget.py
@@ -26,14 +26,46 @@ class OpenGLWidget(QOpenGLWidget):
         self.program = QOpenGLShaderProgram(self)
         self.rotation = 0
 
-        # VAO
+        # VAO/VBO
         self.vao = QOpenGLVertexArrayObject()
+        self.vbo = QOpenGLBuffer()
 
         # Camera
         self.camera = QMatrix4x4()
         self.xCamPos = 0.0
         self.yCamPos = 0.0
         self.zCamPos = 0.0
+
+        # World
+        self.world = QMatrix4x4()
+
+        # Rotation
+        self.xRot = 0
+        self.yRot = 0
+        self.zRot = 0
+
+        # Projection
+        self.proj = QMatrix4x4()
+
+
+    def setup_vertex_attribs(self):
+        _POSITION = 0
+        _COLOR = 1
+
+        position = np.array([-0.5, 0.5, 0.0, -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.5, 0.5, 0.0], np.float32)
+        color = np.array([1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0], np.float32)
+
+        self.vbo.bind()
+
+        glEnableVertexAttribArray(_POSITION)
+        glEnableVertexAttribArray(_COLOR)
+        print("TEST1")
+        glVertexAttribPointer(_POSITION, 3, GL_FLOAT, False, 0, position)
+        print("TEST2")
+        glVertexAttribPointer(_COLOR, 3, GL_FLOAT, False, 0, color)
+        print("TEST3")
+
+        self.vbo.release()
 
     def initializeGL(self):
         # FIXME Are you sure you'll use just one shader for everything?
@@ -51,32 +83,59 @@ class OpenGLWidget(QOpenGLWidget):
 
         self.vao.create()
 
+        # Store the vertex attribute bindings for the program.
+        self.setup_vertex_attribs()
+
         # Our camera has an initial position.
         self.camera.setToIdentity()
         self.camera.translate(self.xCamPos, self.yCamPos, self.zCamPos);
 
         self.program.release()
 
-        ## TODO Delete this scheiße below
-
-        print(self.model.get_vertices())
-        vertices = np.array([0.0, 1.0, -1.0, -1.0, 1.0, -1.0], dtype=np.float32)
-
-        bufferId = glGenBuffers(1)
-        glBindBuffer(GL_ARRAY_BUFFER, bufferId)
-        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, None)
-
     def paintGL(self):
-        painter = QPainter(self)
+        # Clear screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glEnable(GL_DEPTH_TEST)
+        glDisable(GL_CULL_FACE)
 
-        glRotated(self.rotation, 0.0, 1.0, 0.0)
-        glDrawArrays(GL_TRIANGLES, 0, 3)
+        self.world.setToIdentity()
 
-    def resizeGL(self, width, height):
-        super().resizeGL(width, height)
+        # Allow rotation of the world
+        self.world.rotate(self.xRot / 16.0, 1, 0, 0)
+        self.world.rotate(self.yRot / 16.0, 0, 1, 0)
+        self.world.rotate(self.zRot / 16.0, 0, 0, 1)
+
+        # Bind VAO
+        vaoBinder = QOpenGLVertexArrayObject.Binder(self.vao)
+
+        # Bind data of shaders to program
+        self.program.bind();
+        self.program.setUniformValue(self.projMatrixLoc, self.proj);
+        self.program.setUniformValue(self.modelViewMatrixLoc, self.camera * self.world);
+
+        # Load new data only on model data change
+        self.dataAlreadyLoaded = False
+        if (not self.dataAlreadyLoaded):
+
+            # Load data
+            def loadData():
+                pass
+            loadData()
+
+            # Store the vertex attribute bindings for the program.
+            self.setup_vertex_attribs()
+
+            self.dataAlreadyLoaded = True
+
+        # Draw triangulation
+        # Last argument = Number of vertices in total
+        glDrawArrays(GL_TRIANGLES, 0, 3 * 4);
+
+        self.program.release();
+
+    def resizeGL(self, w, h):
+        self.proj.setToIdentity()
+        self.proj.perspective(45.0, (w / h), 0.01, 10000.0)
 
     # Controller dependent on current mode
     def mouseMoveEvent(self, event):
