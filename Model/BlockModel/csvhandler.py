65638c28cbe3ce0fd8bf5b838d365b3774c44a12
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-04-05 16:27:14 -0300

    WIP CSV Parser

--- Model/BlockModel/csvhandler.py
+++ Model/BlockModel/csvhandler.py
@@ -8,20 +8,3 @@ from Model.BlockModel.csvparser import CSVParser
 class CSVHandler(Handler):
     def __init__(self):
         self.parser = CSVParser()
-
-    # Loads a CSV file and updates the model
-    def load_mesh(self, model, filepath):
-        try:
-            self.parser.load_file(filepath)
-            model.vertices = self.parser.get_vertices()
-            model.faces = self.parser.get_faces()
-            return True
-        except Exception:
-            return False
-
-    # Saves an CSV file
-    def save_mesh(self, filepath=None):
-        # TODO Create a new file on filepath
-        # TODO Write the CSV
-        # TODO Close the file
-        return False
--- Model/BlockModel/csvparser.py
+++ Model/BlockModel/csvparser.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python
 
+import csv
 from Model.parser import Parser
 
 
 class CSVParser(Parser):
     def __init__(self):
         super().__init__()
+        self.vertices = []
+        self.indices = []
+        self.values = []
 
-    def load_file(self, filepath: str) -> None:
-        pass
+    def load_file(self, file_path: str) -> None:
+        with open(file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            list_reader = list(reader)
+
+            i = 0
+            CuT = []
+
+            for elem in list_reader:
+                try:
+                    self.vertices.append((float(elem[0]) - 6295, float(elem[1]) - 5950, float(elem[2]) - 2025))
+                    CuT.append(float(elem[3]))
+                    self.indices.append(i)
+                    i += 1
+                except ValueError:
+                    continue
+
+            _CuT = self.normalize(CuT, min(CuT), max(CuT))
+            for c in _CuT:
+                r = min(1.0, 2 * (1.0 - c))
+                g = min(1.0, 2 * c)
+                b = 0
+                self.values.append((r, g, b))
+
+    def get_indices(self) -> list:
+        return self.indices
+
+    def normalize(self, l: list, m: float, M: float) -> list:
+        return [((x - m)/(M - m)) for x in l]
