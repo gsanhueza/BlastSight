69c8a1556c16c39e35f152495bcf959b7a9eda6f
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-04-17 16:01:42 -0400

    WIP Added to mainwindow

--- View/mainwindow.py
+++ View/mainwindow.py
@@ -9,13 +9,25 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         uic.loadUi('View/UI/mainwindow.ui', self)
-
         self.statusBar.showMessage('Ready')
-
-        self.tree_widget = QTreeWidget()
 
     def set_model(self, model):
         self.viewer.set_model(model)
+
+    def fill_tree_widget(self):
+        # Tree widget
+        self.treeWidget.clear()
+
+        for id_, mesh in self.viewer.model.get_mesh_collection():
+            item = QTreeWidgetItem(self.treeWidget)
+            item.setText(0, str(id_))
+            self.treeWidget.addTopLevelItem(item)
+
+        def click(item, col):
+            id_ = int(item.text(0))
+            self.viewer.toggle_wireframe(id_)
+
+        self.treeWidget.itemClicked.connect(click)
 
     # Unless explicitly otherwise, slots are connected via Qt Designer
     def load_mesh_slot(self):
@@ -25,10 +37,12 @@ class MainWindow(QMainWindow):
             directory='.',
             filter='DXF Files (*.dxf);;OFF Files (*.off);;All files (*.*)')
 
-        if self.viewer.add_mesh(filepath):
+        if self.viewer.add_mesh(filepath) != -1:
             self.statusBar.showMessage('Mesh loaded')
         else:
             self.statusBar.showMessage('Cannot load mesh')
+
+        self.fill_tree_widget()
 
     def load_block_model_slot(self):
         # TODO Use QSettings (or something) to remember last directory
@@ -52,24 +66,9 @@ class MainWindow(QMainWindow):
         self.viewer.set_free_mode()
 
     def help_slot(self):
-        # QMessageBox.information(self,
-        #                         'MineVis - Help',
-        #                         'TO-DO: Create help message box')
-
-        # Tree widget
-        self.tree_widget.clear()
-
-        for id_, mesh in self.viewer.model.get_mesh_collection():
-            item = QTreeWidgetItem(self.tree_widget)
-            item.setText(0, str(id_))
-            self.tree_widget.addTopLevelItem(item)
-
-        def click(item, col):
-            id_ = int(item.text(0))
-            self.viewer.toggle_wireframe(id_)
-
-        self.tree_widget.itemClicked.connect(click)
-        self.tree_widget.show()
+        QMessageBox.information(self,
+                                'MineVis - Help',
+                                'TO-DO: Create help message box')
 
     def toggle_wireframe(self, id_: int = 0):
         try:
