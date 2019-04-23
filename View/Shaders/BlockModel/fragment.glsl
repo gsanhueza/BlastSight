23bd9e798838702b228a264b8d85fc4ff747decb
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-04-23 12:12:13 -0400

    Shaders added in block model, TODO Optimize normal calculations

--- View/Shaders/BlockModel/fragment.glsl
+++ View/Shaders/BlockModel/fragment.glsl
@@ -1,10 +1,33 @@
 #version 150
 #extension GL_ARB_separate_shader_objects : enable
 
+layout (location = 0) in vec3 v_position;
 layout (location = 1) in vec3 v_color;
+layout (location = 2) in vec3 v_normal;
+
 out vec4 out_color;
 
 void main()
 {
-    out_color = vec4(v_color, 1.0);
+    float brightness = 100.0;
+    vec3 eye_position = vec3(0.0, 0.0, 0.0);
+    vec3 light_position = vec3(0.0, 0.0, -10.0);
+    vec3 light_color = v_color;
+
+    vec3 light_vector = normalize(light_position - v_position);
+    vec3 reflected_light = reflect(light_vector, v_normal);
+    vec3 eye_vector = normalize(eye_position - v_position);
+
+    // Diffuse
+    float Idiff = max(dot(v_normal, light_vector), 0.0);
+
+    // Specular
+    float Ispec = pow(max(dot(eye_vector, reflected_light), 0.0), brightness);
+
+    // Ambient
+    float Iamb = 0.2;
+
+    out_color = vec4(light_color * (Idiff + Ispec + Iamb), 1.0);
+//
+//    out_color = vec4(v_color, 1.0);
 }
--- View/Shaders/BlockModel/geometry.glsl
+++ View/Shaders/BlockModel/geometry.glsl
@@ -8,28 +8,56 @@ layout (triangle_strip, max_vertices = 24) out;
 layout (location = 1) in vec3 v_color[1];
 layout (location = 1) out vec3 f_color;
 
+layout (location = 2) out vec3 normal;
+
 uniform mat4 proj_matrix;
 uniform mat4 model_view_matrix;
 uniform vec2 block_size;
 
 mat4 mvp = proj_matrix * model_view_matrix;
 
+vec3 calculateNormal(vec4 v1, vec4 v2, vec4 v3)
+{
+    vec3 a = (v2 - v1).xyz;
+    vec3 b = (v3 - v1).xyz;
+    vec3 N = normalize(cross(b, a));
+
+    // We're cheating here, but it's a simple way to ensure every normal aims outside (or every normal aims inside)
+    if (length(N + v1.xyz) > length(v1.xyz))
+    {
+        N = -N;
+    }
+
+    return N;
+}
+
 void AddQuad(vec4 center, vec4 dy, vec4 dx)
 {
-    gl_Position = center + (dx - dy);
+    vec4 v1 = center + (dx - dy);
+    vec4 v2 = center + (-dx - dy);
+    vec4 v3 = center + (dx + dy);
+    vec4 v4 = center + (-dx + dy);
+
+    vec3 N = calculateNormal(v1, v2, v3);
+
+    gl_Position = v1;
     f_color = v_color[0];
+    normal = N;
     EmitVertex();
 
-    gl_Position = center + (-dx - dy);
+    gl_Position = v2;
     f_color = v_color[0];
+    normal = N;
     EmitVertex();
 
-    gl_Position = center + (dx + dy);
+    gl_Position = v3;
     f_color = v_color[0];
+    normal = N;
     EmitVertex();
 
-    gl_Position = center + (-dx + dy);
+    gl_Position = v4;
     f_color = v_color[0];
+    normal = N;
     EmitVertex();
 
     EndPrimitive();
