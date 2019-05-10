6084a1b321a69c16336cb2e55686024bc1f616db
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-05-10 17:27:29 -0400

    Headers detected, implementing PyTest

--- example.py
+++ example.py
@@ -4,7 +4,6 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 from View.openglwidget import OpenGLWidget
-from Model.model import Model
 
 
 class MineVisViewer(OpenGLWidget):
@@ -18,40 +17,21 @@ class MineVisViewer(OpenGLWidget):
         sys.exit(self.app.exec_())
 
 
-def test_model():
-    model = Model()
-    input('Before load')
-    id_off = model.add_mesh('Model/Mesh/caseron.off')
-    input('After load')
-    model.update_mesh(id_off, '/home/gabriel/xyzrgb_statuette.off')
-    input('After update')
-    model.update_mesh(id_off, '/home/gabriel/xyzrgb_statuette.off')
-    input('After update')
-    model.delete_mesh(id_off)
-    input('After delete')
-    id_off = model.add_mesh('/home/gabriel/xyzrgb_statuette.off')
-    input('After big load again')
-    model.delete_mesh(id_off)
-    input('After delete')
-
-
 def test_viewer():
     viewer = MineVisViewer()
-    input('Before load')
     id_off = viewer.add_mesh('/home/gabriel/xyzrgb_statuette.off')
-    input('After load')
     viewer.delete_mesh(id_off)
-    input('After delete')
-    id_off = viewer.add_mesh('/home/gabriel/xyzrgb_statuette.off')
-    input('After load')
-    viewer.delete_mesh(id_off)
-    input('After delete')
-    id_off = viewer.add_mesh('/home/gabriel/xyzrgb_statuette.off')
-    input('After load')
-    viewer.delete_mesh(id_off)
-    input('After delete')
+    viewer.show()
+
+
+def test_block_model():
+    viewer = MineVisViewer()
+    id_csv = viewer.add_block_model('Model/BlockModel/complex.csv')
+    bm = viewer.model.get_block_model(id_csv)
+    bm.set_current_value_string('Au')
+    bm.update_values()
     viewer.show()
 
 
 if __name__ == '__main__':
-    test_viewer()
+    test_block_model()
