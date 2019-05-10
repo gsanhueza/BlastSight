887c15d9e6e0b39d1e7eddd30204e73819bf0476
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-05-10 18:20:08 -0400

    Parser test updated

--- tests/Model/test_parser.py
+++ tests/Model/test_parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pytest
+from Model.Mesh.meshelement import MeshElement
 from Model.parser import Parser
 from Model.Mesh.offparser import OFFParser
 from Model.Mesh.dxfparser import DXFParser
@@ -11,11 +12,27 @@ class TestParser:
         return Parser()
 
 
+class TestOFFParser(TestParser):
+    def generate(self):
+        return OFFParser()
+
+    def test_load_file(self):
+        parser = self.generate()
+        element = MeshElement()
+        parser.load_file('tests/caseron.off', element)
+
+        assert element.get_vertices().size == 3 * 12
+        assert element.get_indices().size == 3 * 20
+
+
 class TestDXFParser(TestParser):
     def generate(self):
         return DXFParser()
 
+    def test_load_file(self):
+        parser = self.generate()
+        element = MeshElement()
+        parser.load_file('tests/caseron.dxf', element)
 
-class TestOFFParser(TestParser):
-    def generate(self):
-        return OFFParser()
+        assert element.get_vertices().size == 3 * 12
+        assert element.get_indices().size == 3 * 20
