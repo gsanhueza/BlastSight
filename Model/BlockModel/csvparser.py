8f9f185d0649fe61842ec13d9597d86c26bb562c
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-05-10 12:05:16 -0400

    Block model parser loads by header name instead of header position

--- Model/BlockModel/csvparser.py
+++ Model/BlockModel/csvparser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import numpy as np
+import csv
 from Model.modelelement import ModelElement
 from Model.parser import Parser
 
@@ -9,53 +9,36 @@ class CSVParser(Parser):
     def __init__(self):
         super().__init__()
 
-        # Data positions by column
-        self.x_pos = 0
-        self.y_pos = 1
-        self.z_pos = 2
-
-    # TODO Not every CSV file comes with x in the first column...
-    def load_csv_scheme(self, file_path: str) -> None:
-        self.x_pos = 0
-        self.y_pos = 1
-        self.z_pos = 2
-
     def load_file(self, file_path: str, model: ModelElement) -> None:
-        vertices = []
-        indices = []
-        values = []
+        with open(file_path, newline='') as f:
+            reader = csv.DictReader(f)
+            lr = list(reader)
+            vals = list(zip(*map(lambda d: d.values(), lr)))
+            data = dict(zip(lr[0], map(list, vals)))
 
-        CuT = []
-        idx = 0
-        for elem in np.loadtxt(file_path, delimiter=',', skiprows=1):
-            try:
-                vertices.append((float(elem[self.x_pos]),
-                                 float(elem[self.y_pos]),
-                                 float(elem[self.z_pos])))
-                CuT.append(float(elem[3]))
-            except ValueError:
-                continue
-
+        # FIXME Those hardcoded strings are in the header of the CSV file
+        x = [float(s) for s in data['x']]
+        y = [float(s) for s in data['y']]
+        z = [float(s) for s in data['z']]
+        CuT = [float(s) for s in data['CuT']]
         min_CuT = min(CuT)
         max_CuT = max(CuT)
 
+        values = []
         for cut in CuT:
             values.append(
                 (min(1.0, 2 * (1.0 - CSVParser.normalize(cut, min_CuT, max_CuT))),
                  min(1.0, 2 * CSVParser.normalize(cut, min_CuT, max_CuT)),
                  0.0)
             )
-            indices.append(idx)
-            idx += 1
 
-        # Model data
+        vertices = list(zip(x, y, z))
+        indices = list(range(3 * len(vertices)))
+
         model.set_vertices(vertices)
         model.set_indices(indices)
         model.set_values(values)
 
     @staticmethod
     def normalize(x: float, min_val: float, max_val: float) -> float:
-        try:
-            return (x - min_val)/(max_val - min_val)
-        except ZeroDivisionError:
-            return 1
+        return (x - min_val)/(max_val - min_val) if max_val != min_val else 0
