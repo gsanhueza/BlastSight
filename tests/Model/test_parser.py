fbb58e12d21710254fb410a6aacae579f878dde8
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-05-12 19:24:56 -0400

    Block model examples moved to tests folder

--- tests/Model/test_parser.py
+++ tests/Model/test_parser.py
@@ -2,9 +2,11 @@
 
 import pytest
 from Model.Mesh.meshelement import MeshElement
+from Model.BlockModel.blockmodelelement import BlockModelElement
 from Model.parser import Parser
 from Model.Mesh.offparser import OFFParser
 from Model.Mesh.dxfparser import DXFParser
+from Model.BlockModel.csvparser import CSVParser
 
 
 class TestParser:
@@ -36,3 +38,16 @@ class TestDXFParser(TestParser):
 
         assert len(list(element.get_vertices().tolist())) == 12
         assert len(list(element.get_indices().tolist())) == 20
+
+
+class TestCSVParser(TestParser):
+    def generate(self):
+        return CSVParser()
+
+    def test_load_file(self):
+        parser = self.generate()
+        element = BlockModelElement()
+        parser.load_file('tests/mini.csv', element)
+
+        assert len(list(element.get_vertices().tolist())) == 6
+        assert len(list(element.get_indices().tolist())) == 18
