1e8c1c739f0836d7d21cab7ef4cdc3655287625c
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-05-18 17:39:40 -0400

    Main can be called with arguments

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 
-from PyQt5.QtWidgets import QApplication
-from View.GUI.minevis import MineVis
+
+def application():
+    from PyQt5.QtWidgets import QApplication
+    from View.GUI.minevis import MineVis
+
+    qt_app = QApplication(sys.argv)
+
+    w = MineVis()
+    w.show()
+
+    sys.exit(qt_app.exec_())
+
+
+def standalone():
+    from View.standaloneviewer import viewer
+
+    viewer.show()
+
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    parser = argparse.ArgumentParser(description='Starts MineVis in app or standalone viewer mode.')
+    parser.add_argument('--app', action='store_true', help='Starts in App mode.')
+    parser.add_argument('--viewer', action='store_true', help='Starts in Viewer mode.')
 
-    window = MineVis()
+    args = parser.parse_args()
 
-    window.show()
-    sys.exit(app.exec_())
+    if args.viewer and not args.app:
+        standalone()
+    elif args.app or not args.viewer:
+        application()
