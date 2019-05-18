d2e000558e8de8fea2f976f77826ff12871640d5
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-05-18 17:27:18 -0400

    Stand-alone viewer example added

---
+++ View/GUI/standaloneviewer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from View.GUI.openglwidget import OpenGLWidget
+
+
+class StandaloneViewer(OpenGLWidget):
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        super().__init__()
+        self.setWindowTitle('MineVis: Stand-alone Viewer')
+
+    def show(self):
+        super().show()
+        sys.exit(self.app.exec_())
+
+
+viewer = StandaloneViewer()
