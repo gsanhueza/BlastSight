1505c758de91b406d0ae1a4633ea89890b74be0b
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-05-18 19:11:49 -0400

    Command queue added (needed when there is a need to modify a Drawable

--- View/standaloneviewer.py
+++ View/standaloneviewer.py
@@ -9,11 +9,30 @@ from View.GUI.openglwidget import OpenGLWidget
 class StandaloneViewer(OpenGLWidget):
     def __init__(self):
         self.app = QApplication(sys.argv)
+        self.command_queue = []
+
         super().__init__()
         self.setWindowTitle('MineVis: Stand-alone Viewer')
 
+    def show_element(self, id_: int) -> None:
+        self.command_queue.append(lambda: OpenGLWidget.show_element(self, id_))
+
+    def hide_element(self, id_: int) -> None:
+        self.command_queue.append(lambda: OpenGLWidget.hide_element(self, id_))
+
+    def delete_element(self, id_: int) -> None:
+        self.command_queue.append(lambda: OpenGLWidget.delete_element(self, id_))
+
+    def toggle_wireframe(self, id_: int) -> None:
+        self.command_queue.append(lambda: OpenGLWidget.toggle_wireframe(self, id_))
+
     def show(self):
         super().show()
+
+        # Apply commands in queue
+        for command in self.command_queue:
+            command()
+
         sys.exit(self.app.exec_())
 
 
