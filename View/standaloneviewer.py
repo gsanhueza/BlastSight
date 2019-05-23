e3d9cfa517667e77c697c10870f53e3e5335553b
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-05-23 18:05:23 -0400

    Standalone viewer cleanup after miracle fix

--- View/standaloneviewer.py
+++ View/standaloneviewer.py
@@ -10,27 +10,8 @@ class StandaloneViewer(OpenGLWidget):
     def __init__(self):
         self.app = QApplication(sys.argv)
         super().__init__()
-        self.app_mode = False
-        self.command_queue = []
         self.setWindowTitle('MineVis (Standalone)')
-
-    # def show_drawable(self, id_: int) -> None:
-    #     self.command_queue.append(lambda this: OpenGLWidget.show_drawable(this, id_))
-    #
-    # def hide_drawable(self, id_: int) -> None:
-    #     self.command_queue.append(lambda this: OpenGLWidget.hide_drawable(this, id_))
-    #
-    # def delete(self, id_: int) -> None:
-    #     self.command_queue.append(lambda this: OpenGLWidget.delete(this, id_))
-    #
-    # def toggle_wireframe(self, id_: int) -> None:
-    #     self.command_queue.append(lambda this: OpenGLWidget.toggle_wireframe(this, id_))
 
     def show(self):
         super().show()
-
-        # Apply commands in queue
-        for command in self.command_queue:
-            command(self)
-
         sys.exit(self.app.exec_())
