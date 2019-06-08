f852113ca442d893dc959ff4378e0c3cc95693bc
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-06-08 13:30:12 -0400

    Simplified GLDrawableCollection

--- libraries/View/Drawables/drawablecollection.py
+++ libraries/View/Drawables/drawablecollection.py
@@ -4,39 +4,16 @@ from collections import OrderedDict
 from .gldrawable import GLDrawable
 
 
-class GLDrawableCollection:
+class GLDrawableCollection(OrderedDict):
     def __init__(self):
         super().__init__()
-        self.drawable_dict = OrderedDict()
-
-    def __getitem__(self, key: int):
-        return self.drawable_dict[key]
-
-    def __setitem__(self, key: int, value: GLDrawable) -> None:
-        self.drawable_dict[key] = value
-
-    def __len__(self) -> int:
-        return self.drawable_dict.__len__()
-
-    def __delitem__(self, key):
-        self.drawable_dict.__delitem__(key)
-
-    def items(self):
-        return self.drawable_dict.items()
-
-    """
-    Custom methods
-    """
 
     def add(self, id_: int, drawable: GLDrawable) -> None:
-        self.drawable_dict[id_] = drawable
+        self.__setitem__(id_, drawable)
 
     def draw(self, proj_matrix, view_matrix, model_matrix) -> None:
-        for drawable in self.drawable_dict.values():
+        for drawable in self.values():
             if not drawable.is_initialized:
                 drawable.initialize()
 
             drawable.draw(proj_matrix, view_matrix, model_matrix)
-
-    def clear(self) -> None:
-        self.drawable_dict.clear()
