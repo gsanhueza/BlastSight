780bee4872a56e0645087b5b49bde2eae4877812
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-06-09 01:09:24 -0400

    Lighting bias in blockmodelgl shader

--- libraries/View/Shaders/BlockModel/fragment.glsl
+++ libraries/View/Shaders/BlockModel/fragment.glsl
@@ -19,14 +19,17 @@ vec3 lambert(vec3 N, vec3 L, vec3 color)
 
 void main()
 {
-    vec3 light_position = vec3(0.0, 0.0, 100000.0);
+    vec3 light_position_front = vec3(0.0, 0.0, 100000.0);
+    vec3 light_position_up = vec3(0.0, 100000.0, 0.0);
     vec3 light_color = v_color;
 
     vec3 X = dFdx(pos_mv);
     vec3 Y = dFdy(pos_mv);
     vec3 v_normal = normalize(cross(X, Y));
 
-    vec3 col = lambert(v_normal, light_position, light_color);
+    float front_light_bias = 0.85;
+    vec3 color_front = lambert(v_normal, light_position_front, light_color);
+    vec3 color_up = lambert(v_normal, light_position_up, light_color);
 
-    out_color = vec4(0.05 + col, 1.0);
+    out_color = vec4(0.05 + (front_light_bias * color_front) + ((1 - front_light_bias) * color_up), 1.0);
 }
