902e81d896486ef487d322272cdca763c3031e5e
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-06-26 17:38:42 -0400

    Wireframe meshes added in refactor

--- libraries/View/Drawables/gldrawablecollection.py
+++ libraries/View/Drawables/gldrawablecollection.py
@@ -5,12 +5,14 @@ from .gldrawable import GLDrawable
 from .meshgl import MeshGL
 
 from .meshprogram import MeshProgram
+from .wireframeprogram import WireframeProgram
 
 
 class GLDrawableCollection(OrderedDict):
     def __init__(self, widget):
         super().__init__()
         self.mesh_program = MeshProgram(widget)
+        self.wireframe_program = WireframeProgram(widget)
 
     def add(self, id_: int, drawable: GLDrawable) -> None:
         self.__setitem__(id_, drawable)
@@ -23,13 +25,19 @@ class GLDrawableCollection(OrderedDict):
         self.mesh_program.update_uniform('proj_matrix', proj_matrix)
         self.mesh_program.update_uniform('model_view_matrix', view_matrix * model_matrix)
 
-        for drawable in self.get_meshes():
-            self.mesh_program.update_uniform('u_color',
-                                             float(drawable.color[0]),
-                                             float(drawable.color[1]),
-                                             float(drawable.color[2]),
-                                             drawable.alpha)
+        for drawable in self.normal_meshes:
+            self.mesh_program.update_uniform('u_color', float(drawable.color[0]), float(drawable.color[1]), float(drawable.color[2]), drawable.alpha)
+            drawable.draw(proj_matrix, view_matrix, model_matrix)
 
+        # Wireframes
+        self.wireframe_program.setup_program()
+        self.wireframe_program.bind()
+
+        self.wireframe_program.update_uniform('proj_matrix', proj_matrix)
+        self.wireframe_program.update_uniform('model_view_matrix', view_matrix * model_matrix)
+
+        for drawable in self.wireframe_meshes:
+            self.wireframe_program.update_uniform('u_color', float(drawable.color[0]), float(drawable.color[1]), float(drawable.color[2]), drawable.alpha)
             drawable.draw(proj_matrix, view_matrix, model_matrix)
 
         # Others
@@ -41,6 +49,14 @@ class GLDrawableCollection(OrderedDict):
 
     def get_meshes(self):
         return list(filter(lambda x: isinstance(x, MeshGL), self.values()))
+
+    @property
+    def normal_meshes(self):
+        return list(filter(lambda x: not x.wireframe_enabled, self.get_meshes()))
+
+    @property
+    def wireframe_meshes(self):
+        return list(filter(lambda x: x.wireframe_enabled, self.get_meshes()))
 
     def get_others(self):
         return list(filter(lambda x: not isinstance(x, MeshGL), self.values()))
