530b87f11382582525946a67ab9b4a9d2180a684
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-06-27 17:46:46 -0400

    Init mouse movement speed based on camera distance

--- libraries/Controller/normalmode.py
+++ libraries/Controller/normalmode.py
@@ -22,8 +22,11 @@ class NormalMode(Mode):
             self.set_x_rotation(self.widget.xWorldRot + dy)
             self.set_y_rotation(self.widget.yWorldRot + dx)
         elif event.buttons() == Qt.MiddleButton:
-            self.set_x_movement(self.widget.xWorldPos + (dx / 200.0))
-            self.set_y_movement(self.widget.yWorldPos - (dy / 200.0))
+            # FIXME Dependent on aspect ratio
+            distance_x = 200 * abs(self.widget.zWorldPos + self.widget.centroid[2]) / self.widget.width()
+            distance_y = 200 * abs(self.widget.zWorldPos + self.widget.centroid[2]) / self.widget.height()
+            self.set_x_movement(self.widget.xWorldPos + (distance_x * dx / 200.0))
+            self.set_y_movement(self.widget.yWorldPos - (distance_y * dy / 200.0))
         elif event.buttons() == Qt.RightButton:
             self.set_x_rotation(self.widget.xWorldRot + dy)
             self.set_z_rotation(self.widget.zWorldRot - dx)
