4dea1f4c530fbecb6bf459d6b8a6e1502f152a84
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-07-03 17:36:29 -0400

    Minimized lambert calculation in shader

--- libraries/View/Shaders/Mesh/fragment.glsl
+++ libraries/View/Shaders/Mesh/fragment.glsl
@@ -4,23 +4,16 @@ in vec3 pos_mv;
 in vec4 v_color;
 out vec4 out_color;
 
-float lambertian(vec3 N, vec3 L)
-{
-    vec3 normalized_N = normalize(N);
-    vec3 normalized_L = normalize(L);
-    return max(dot(normalized_N, normalized_L), 0.0);
-}
-
 vec3 lambert(vec3 N, vec3 L, vec3 color)
 {
-    return color * lambertian(N, L);
+    return color * max(dot(normalize(N), normalize(L)), 0.0);
 }
 
 void main()
 {
     vec3 X = dFdx(pos_mv);
     vec3 Y = dFdy(pos_mv);
-    vec3 v_normal = normalize(cross(X, Y));
+    vec3 v_normal = cross(X, Y);
 
     vec3 light_position = vec3(0.0, 0.0, 1000.0);
     vec3 col = lambert(v_normal, light_position, v_color.rgb);
