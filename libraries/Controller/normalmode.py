b120736a909759ffff4bceb77febcd88726e4c53
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-07-15 17:18:29 -0400

    Quick fix rotation orientation

--- libraries/Controller/normalmode.py
+++ libraries/Controller/normalmode.py
@@ -17,18 +17,18 @@ class NormalMode(Mode):
         dy = event.y() - self.lastPos.y()
 
         widget.world.setToIdentity()
-        if event.buttons() == Qt.RightButton:
+        if event.buttons() == Qt.LeftButton:
             self.set_x_rotation(widget, widget.xWorldRot + dy)
-            self.set_y_rotation(widget, widget.yWorldRot + dx)
+            self.set_z_rotation(widget, widget.zWorldRot + dx)
+        elif event.buttons() == Qt.RightButton:
+            self.set_x_rotation(widget, widget.xWorldRot + dy)
+            self.set_y_rotation(widget, widget.yWorldRot - dx)
         elif event.buttons() == Qt.MiddleButton:
             # FIXME Dependent on aspect ratio
             distance_x = 200 * abs(widget.zCameraPos + widget.centroid[2]) / widget.width()
             distance_y = 200 * abs(widget.zCameraPos + widget.centroid[2]) / widget.height()
             self.set_x_movement(widget, widget.xCameraPos + (distance_x * dx / 200.0))
             self.set_y_movement(widget, widget.yCameraPos - (distance_y * dy / 200.0))
-        elif event.buttons() == Qt.LeftButton:
-            self.set_x_rotation(widget, widget.xWorldRot + dy)
-            self.set_z_rotation(widget, widget.zWorldRot - dx)
 
         self.lastPos = QPoint(event.pos())
 
