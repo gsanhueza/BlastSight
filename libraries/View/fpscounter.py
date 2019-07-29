3049d040d4ec26b64ed298afe5ba2227e3351506
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-07-29 17:18:10 -0400

    Revert simplification, but signals stay

--- libraries/View/fpscounter.py
+++ libraries/View/fpscounter.py
@@ -7,8 +7,12 @@ class FPSCounter:
     def __init__(self):
         self.start_time = time.time()
         self.fps = 0.0
+        self.resolution = 1.0
+        self.counter = 0
 
     def tick(self):
-        diff = time.time() - self.start_time
-        self.fps = 1.0 / diff
-        self.start_time = time.time()
+        self.counter += 1
+        if time.time() - self.start_time > self.resolution:
+            self.fps = self.counter
+            self.counter = 0
+            self.start_time = time.time()
--- libraries/View/tests/test_fpscounter.py
+++ libraries/View/tests/test_fpscounter.py
@@ -7,5 +7,20 @@ class TestFPSCounter:
     def test_tick(self):
         counter = FPSCounter()
         assert counter.fps == 0.0
+        assert counter.counter == 0
+        assert counter.resolution == 1.0
         counter.tick()
-        assert counter.fps > 0.0
+        assert counter.counter == 1
+
+    def test_resolution(self):
+        counter = FPSCounter()
+        counter.resolution = 0.0
+        assert counter.fps == 0.0
+        assert counter.counter == 0
+        assert counter.resolution == 0.0
+
+        counter.tick()
+
+        assert counter.fps != 0.0
+        assert counter.counter == 0
+        assert counter.resolution == 0.0
