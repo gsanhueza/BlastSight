3fd2635b3d55d121723f7ea28d973abd4a275ab2
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-08-07 16:12:20 -0400

    AABB early detection when detecting ray-mesh intersection

--- libraries/Model/tests/test_utils.py
+++ libraries/Model/tests/test_utils.py
@@ -5,20 +5,30 @@ from ..Elements.meshelement import MeshElement
 
 
 class TestUtils:
-    mesh = np.array([[-1.0, 0.0, 0.0],
-                     [1.0, 0.0, 0.0],
-                     [0.0, 1.0, 0.0],
-                     [0.0, -1.0, 0.0]])
+    # Triangle
+    vertices = np.array([[-1.0, 0.0, 0.0],
+                         [1.0, 0.0, 0.0],
+                         [0.0, 1.0, 0.0],
+                         [0.0, -1.0, 0.0]])
 
-    triangle = mesh.view(np.ndarray)[[0, 1, 2]]
-    triangle_low = mesh.view(np.ndarray)[[0, 3, 1]]
-    triangle_degenerate = mesh.view(np.ndarray)[[0, 0, 0]]
+    triangle = vertices.view(np.ndarray)[[0, 1, 2]]
+    triangle_low = vertices.view(np.ndarray)[[0, 3, 1]]
+    triangle_degenerate = vertices.view(np.ndarray)[[0, 0, 0]]
 
-    mesh_element = MeshElement(vertices=mesh, indices=[[0, 1, 2], [0, 3, 1]])
+    mesh_element = MeshElement(vertices=vertices, indices=[[0, 1, 2], [0, 3, 1]])
+
+    # Tetrahedron
+    vertices = np.array([[-1.0, 0.0, 0.0],
+                         [1.0, 0.0, 0.0],
+                         [0.0, 1.0, 0.0],
+                         [0.0, 0.5, 0.5]])
+    indices = [[0, 1, 2], [0, 1, 3], [1, 2, 3], [2, 0, 3]]
+
+    tetrahedron = MeshElement(vertices=vertices, indices=indices)
 
     origin = np.array([0.0, 0.5, 10.0])
-    origin_low = np.array([0.0, -0.1, 1.0])
-    origin_translated = np.array([10.0, 0.5, 10.0])
+    origin_low = np.array([0.0, -0.1, 10.0])
+    origin_translated = np.array([10.0, 0.5, 1.0])
 
     ray = np.array([0.0, 0.0, -1.0])
     ray_reversed = np.array([0.0, 0.0, 1.0])
@@ -27,19 +37,29 @@ class TestUtils:
     ray_perpendicular = np.array([1.0, 0.0, 0.0])
 
     def test_mesh_intersection(self):
-        assert mesh_intersection(self.origin, self.ray, self.mesh_element)
-        assert not mesh_intersection(self.origin, self.ray_reversed, self.mesh_element)
-        assert not mesh_intersection(self.origin, self.ray_low, self.mesh_element)
-        assert not mesh_intersection(self.origin, self.ray_perpendicular, self.mesh_element)
-        assert not mesh_intersection(self.origin_translated, self.ray, self.mesh_element)
-        assert not mesh_intersection(self.origin, self.ray_oblique, self.mesh_element)
+        assert mesh_intersection(self.origin, self.ray, self.mesh_element).__len__() > 0
+        assert mesh_intersection(self.origin, self.ray_reversed, self.mesh_element).__len__() == 0
+        assert mesh_intersection(self.origin, self.ray_low, self.mesh_element).__len__() == 0
+        assert mesh_intersection(self.origin, self.ray_perpendicular, self.mesh_element).__len__() == 0
+        assert mesh_intersection(self.origin_translated, self.ray, self.mesh_element).__len__() == 0
+        assert mesh_intersection(self.origin_translated, self.ray_oblique, self.mesh_element).__len__() == 0
+        assert mesh_intersection(self.origin, self.ray_oblique, self.mesh_element).__len__() == 0
+        assert mesh_intersection(self.origin_translated, self.ray, self.tetrahedron).__len__() == 0
 
     def test_triangle_intersection(self):
-        assert triangle_intersection(self.origin, self.ray, self.triangle)
-        assert not triangle_intersection(self.origin, self.ray_reversed, self.triangle)
-        assert not triangle_intersection(self.origin, self.ray_low, self.triangle_low)
-        assert not triangle_intersection(self.origin, self.ray_low, self.triangle)
-        assert not triangle_intersection(self.origin_translated, self.ray, self.triangle)
-        assert not triangle_intersection(self.origin, self.ray_oblique, self.triangle)
-        assert not triangle_intersection(self.origin, self.ray, self.triangle_degenerate)
-        assert not triangle_intersection(self.origin_low, self.ray, self.triangle)
+        assert triangle_intersection(self.origin, self.ray, self.triangle) is not None
+        assert triangle_intersection(self.origin, self.ray_reversed, self.triangle) is None
+        assert triangle_intersection(self.origin, self.ray_low, self.triangle_low) is None
+        assert triangle_intersection(self.origin, self.ray_low, self.triangle) is None
+        assert triangle_intersection(self.origin_translated, self.ray, self.triangle) is None
+        assert triangle_intersection(self.origin, self.ray_oblique, self.triangle) is None
+        assert triangle_intersection(self.origin, self.ray, self.triangle_degenerate) is None
+        assert triangle_intersection(self.origin_low, self.ray, self.triangle) is None
+
+    def test_aabb_intersection(self):
+        assert aabb_intersection(self.origin, self.ray, self.tetrahedron)
+        assert not aabb_intersection(self.origin, self.ray_low, self.tetrahedron)
+        assert not aabb_intersection(self.origin, self.ray_perpendicular, self.tetrahedron)
+        assert not aabb_intersection(self.origin_translated, self.ray, self.tetrahedron)
+        assert not aabb_intersection(self.origin_translated, self.ray_oblique, self.tetrahedron)
+        assert not aabb_intersection(self.origin, self.ray_oblique, self.tetrahedron)
--- libraries/Model/utils.py
+++ libraries/Model/utils.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python
 
 import numpy as np
-import operator
-from functools import reduce, partial
+from functools import partial
 
 from .Elements.meshelement import MeshElement
 
 
-def mesh_intersection(origin: np.ndarray, ray: np.ndarray, mesh: MeshElement) -> bool:
+def mesh_intersection(origin: np.ndarray, ray: np.ndarray, mesh: MeshElement) -> list:
+    # Early detection test
+    if not aabb_intersection(origin, ray, mesh):
+        return []
+
     curry_triangle = partial(partial(triangle_intersection, origin), ray)
 
     triangles = mesh.vertices.view(np.ndarray)[mesh.indices]
     results = map(curry_triangle, triangles)
 
-    return reduce(operator.__or__, results)
+    return [x for x in results if x is not None]
 
 
-def triangle_intersection(origin: np.ndarray, ray: np.ndarray, triangle: np.ndarray) -> bool:
+def aabb_intersection(origin, ray, mesh):
+    # Adapted from https://tavianator.com/fast-branchless-raybounding-box-intersections-part-2-nans/
+    b_min = np.array([mesh.x.min(), mesh.y.min(), mesh.z.min()])
+    b_max = np.array([mesh.x.max(), mesh.y.max(), mesh.z.max()])
+    b_diff = b_max - b_min
+
+    if b_diff.min() == 0.0:  # Flat mesh means AABB unreliable
+        return True
+
+    # Result of division by zero used deliberately
+    with np.errstate(divide='ignore'):
+        ray_inv = 1.0 / ray
+
+    t1 = (b_min[0] - origin[0]) * ray_inv[0]
+    t2 = (b_max[0] - origin[0]) * ray_inv[0]
+
+    tmin = min(t1, t2)
+    tmax = max(t1, t2)
+
+    for i in range(1, 3):
+        t1 = (b_min[i] - origin[i]) * ray_inv[i]
+        t2 = (b_max[i] - origin[i]) * ray_inv[i]
+
+        tmin = max(tmin, min(t1, t2))
+        tmax = min(tmax, max(t1, t2))
+
+    return tmax > max(tmin, 0.0)
+
+
+def triangle_intersection(origin: np.ndarray, ray: np.ndarray, triangle: np.ndarray):
     # Idea taken from https://cadxfem.org/inf/Fast%20MinimumStorage%20RayTriangle%20Intersection.pdf
     # Code adapted from https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
     _EPSILON = 0.0000001
@@ -31,26 +63,27 @@ def triangle_intersection(origin: np.ndarray, ray: np.ndarray, triangle: np.ndar
     a = np.dot(edge1, h)
 
     if -_EPSILON < a < _EPSILON:
-        return False  # This ray is parallel to this triangle.
+        return None  # This ray is parallel to this triangle.
 
     f = 1.0 / a
     s = origin - vertex0
     u = f * np.dot(s, h)
 
     if u < 0.0 or u > 1.0:
-        return False
+        return None
 
     q = np.cross(s, edge1)
     v = f * np.dot(ray, q)
 
     if v < 0.0 or u + v > 1.0:
-        return False
+        return None
 
     # At this stage we can compute t to find out where the intersection point is on the line.
     t = f * np.dot(edge2, q)
 
     if t > _EPSILON:  # ray intersection
         intersection_point = origin + ray * t  # Here is the intersection point
-        return True
-    else:  # This means that there is a line intersection but not a ray intersection.
-        return False
+        return intersection_point
+
+    # This means that there is a line intersection but not a ray intersection.
+    return None
