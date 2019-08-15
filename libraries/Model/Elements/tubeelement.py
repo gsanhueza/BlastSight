87b65c9f8b225990183d5c4ef1a1ec74bb854323
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-08-15 00:24:32 -0400

    Re-implemented cylinder with pymesh, requirements updated

--- libraries/Model/Elements/tubeelement.py
+++ libraries/Model/Elements/tubeelement.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
 import numpy as np
-from random import random
-from trimesh.creation import cylinder
-
 from .element import Element
 
 
@@ -14,7 +11,7 @@ class TubeElement(Element):
         self._resolution = None
 
         assert len(self.vertices) >= 2
-        self.color = kwargs.get('color', [random() for _ in range(3)])
+        self.color = kwargs.get('color', np.random.rand(3))
         self.radius = kwargs.get('radius', 0.15)
         self.resolution = kwargs.get('resolution', 15)
 
@@ -47,10 +44,99 @@ class TubeElement(Element):
         indices = []
         delta = 0
 
-        for v1, v2 in zip(self.vertices[:-1], self.vertices[1:]):
-            c = cylinder(radius=self.radius, sections=self.resolution, segment=[v1, v2])
-            vertices.append(c.vertices)
-            indices.append(c.faces + delta)
-            delta += (c.faces.max() + 1)
+        for v0, v1 in zip(self.vertices[:-1], self.vertices[1:]):
+            c_vertices, c_faces = self.cylinder(self.radius, self.resolution, [v0, v1])
+            vertices.append(c_vertices)
+            indices.append(c_faces + delta)
+            delta += (c_faces.max() + 1)
 
         return np.concatenate(vertices), np.concatenate(indices)
+
+    @staticmethod
+    def cylinder(radius, resolution, segment):
+        # cylinder, quat_from_data and to_matrix taken and adapted from PyMesh
+        Z = np.array([0.0, 0.0, 1.0])
+        p0, p1 = segment
+
+        axis = p1 - p0
+        length = np.linalg.norm(axis)
+
+        if length <= 1e-12:
+            axis = Z
+
+        angles = np.linspace(0, 2 * np.pi, resolution + 1)[:-1]
+        rim = np.column_stack((np.sin(angles), np.cos(angles), np.zeros(angles.size)))
+        rot = TubeElement.to_matrix(TubeElement.quat_from_data(Z, axis))
+
+        bottom_rim = np.dot(rot, rim.T).T * radius + p0
+        top_rim = np.dot(rot, rim.T).T * radius + p1
+
+        vertices = np.vstack([[p0, p1], bottom_rim, top_rim])
+
+        vec_resolution = np.arange(resolution)
+
+        # Bottom fan
+        bottom_fan = np.zeros((resolution, 3))
+        bottom_fan[:, 0] = np.zeros(resolution)
+        bottom_fan[:, 1] = np.remainder(vec_resolution + 1, resolution) + 2
+        bottom_fan[:, 2] = vec_resolution + 2
+
+        # Top fan
+        top_fan = np.zeros((resolution, 3))
+        top_fan[:, 0] = np.ones(resolution)
+        top_fan[:, 1] = vec_resolution + resolution + 2
+        top_fan[:, 2] = np.remainder(vec_resolution + 1, resolution) + resolution + 2
+
+        # Sides
+        side_a = np.zeros((resolution, 3))
+        side_a[:, 0] = vec_resolution + 2
+        side_a[:, 1] = np.remainder(vec_resolution + 1, resolution) + 2
+        side_a[:, 2] = vec_resolution + 2 + resolution
+
+        side_b = np.zeros((resolution, 3))
+        side_b[:, 0] = vec_resolution + 2 + resolution
+        side_b[:, 1] = np.remainder(vec_resolution + 1, resolution) + 2
+        side_b[:, 2] = np.remainder(vec_resolution + 1, resolution) + 2 + resolution
+
+        side = np.append(side_a, side_b).astype(np.int)
+        side = side.reshape((-1, 3), order='C')
+
+        indices = np.vstack([bottom_fan, top_fan, side])
+
+        return vertices, indices
+
+    @staticmethod
+    def quat_from_data(v1, v2):
+        eps = 1e-12
+        v1 /= np.linalg.norm(v1)
+        v2 /= np.linalg.norm(v2)
+        c = np.dot(v1, v2)
+        if c < -1.0 + eps:
+            # v1 is parallel and opposite of v2
+            u, s, v = np.linalg.svd(np.array([v1, v2]))
+            axis = v[2, :]
+        else:
+            axis = np.cross(v1, v2)
+            l = np.linalg.norm(axis)
+            if l > 0.0:
+                axis /= np.linalg.norm(axis)
+            else:
+                # Parallel vectors.
+                axis = v1
+
+        w_sq = 0.5 * (1.0 + c)
+        l = np.sqrt(1.0 - w_sq) * axis
+        quat = np.array([np.sqrt(w_sq), l[0], l[1], l[2]])
+
+        return quat
+
+    @staticmethod
+    def to_matrix(a):
+        return np.array([
+            [1 - 2 * a[2] * a[2] - 2 * a[3] * a[3], 2 * a[1] * a[2] - 2 * a[3] * a[0],
+             2 * a[1] * a[3] + 2 * a[2] * a[0]],
+            [2 * a[1] * a[2] + 2 * a[3] * a[0], 1 - 2 * a[1] * a[1] - 2 * a[3] * a[3],
+             2 * a[2] * a[3] - 2 * a[1] * a[0]],
+            [2 * a[1] * a[3] - 2 * a[2] * a[0], 2 * a[2] * a[3] + 2 * a[1] * a[0],
+             1 - 2 * a[1] * a[1] - 2 * a[2] * a[2]],
+        ])
