a39aca0bbf93a4736c7809da8d5bf0876bbedbe3
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-08-27 16:17:26 -0400

    Solved face-culling ahead of time (was calculating normals in a different space (projection space vs view space)

--- minevis/view/drawables/shaders/Block/geometry.glsl
+++ minevis/view/drawables/shaders/Block/geometry.glsl
@@ -1,10 +1,9 @@
-#version 150
-#extension GL_ARB_separate_shader_objects : enable
-#extension GL_EXT_geometry_shader4 : enable
+#version 330
 
 layout (points) in;
 layout (triangle_strip, max_vertices = 12) out;
 
+in vec3 v_position[1];
 in vec3 v_color[1];
 in float v_alpha[1];
 
@@ -18,32 +17,32 @@ uniform vec3 block_size;
 
 mat4 mvp = proj_matrix * model_view_matrix;
 
-void AddQuad(vec4 center, vec4 dy, vec4 dx, vec3 n)
+void AddQuad(vec4 center, vec4 shift, vec4 dy, vec4 dx, vec3 n)
 {
-    vec4 v1 = center + (dx - dy);
-    vec4 v2 = center + (-dx - dy);
-    vec4 v3 = center + (dx + dy);
-    vec4 v4 = center + (-dx + dy);
+    vec4 v1 = (center + shift) + (dx - dy);
+    vec4 v2 = (center + shift) + (-dx - dy);
+    vec4 v3 = (center + shift) + (dx + dy);
+    vec4 v4 = (center + shift) + (-dx + dy);
 
     // Emit a primitive only if the sign of the dot product is positive
-    vec3 N = -(mvp * vec4(n, 0.0)).xyz;
+    vec3 normal = (model_view_matrix * vec4(n, 0.0)).xyz;
 
-    if (N.z > 0.0)
+    if (dot(-center.xyz, normal) > 0.0)
     {
-        v_normal = N;
+        v_normal = normal;
         f_color = v_color[0];
         f_alpha = v_alpha[0];
 
-        gl_Position = v1;
+        gl_Position = proj_matrix * v1;
         EmitVertex();
 
-        gl_Position = v2;
+        gl_Position = proj_matrix * v2;
         EmitVertex();
 
-        gl_Position = v3;
+        gl_Position = proj_matrix * v3;
         EmitVertex();
 
-        gl_Position = v4;
+        gl_Position = proj_matrix * v4;
         EmitVertex();
 
         EndPrimitive();
@@ -52,15 +51,15 @@ void AddQuad(vec4 center, vec4 dy, vec4 dx, vec3 n)
 
 void main()
 {
-    vec4 center = gl_in[0].gl_Position;
+    vec4 center = model_view_matrix * vec4(v_position[0], 1.0);
+    vec4 dx = 0.5f * model_view_matrix[0] * block_size.x;
+    vec4 dy = 0.5f * model_view_matrix[1] * block_size.y;
+    vec4 dz = 0.5f * model_view_matrix[2] * block_size.z;
 
-    vec4 dx = mvp[0] / 2.0f * block_size.x;
-    vec4 dy = mvp[1] / 2.0f * block_size.y;
-    vec4 dz = mvp[2] / 2.0f * block_size.z;
-    AddQuad(center + dx, dy, dz, vec3(1.0, 0.0, 0.0)); // Right
-    AddQuad(center - dx, dz, dy, vec3(-1.0, 0.0, 0.0)); // Left
-    AddQuad(center + dy, dz, dx, vec3(0.0, 1.0, 0.0)); // Top
-    AddQuad(center - dy, dx, dz, vec3(0.0, -1.0, 0.0)); // Bottom
-    AddQuad(center + dz, dx, dy, vec3(0.0, 0.0, 1.0)); // Front
-    AddQuad(center - dz, dy, dx, vec3(0.0, 0.0, -1.0)); // Back
+    AddQuad(center, +dx, dy, dz, vec3(1.0, 0.0, 0.0));  // Right
+    AddQuad(center, -dx, dz, dy, vec3(-1.0, 0.0, 0.0)); // Left
+    AddQuad(center, +dy, dz, dx, vec3(0.0, 1.0, 0.0));  // Top
+    AddQuad(center, -dy, dx, dz, vec3(0.0, -1.0, 0.0)); // Bottom
+    AddQuad(center, +dz, dx, dy, vec3(0.0, 0.0, 1.0));  // Front
+    AddQuad(center, -dz, dy, dx, vec3(0.0, 0.0, -1.0)); // Back
 }
--- minevis/view/drawables/shaders/Block/vertex.glsl
+++ minevis/view/drawables/shaders/Block/vertex.glsl
@@ -4,6 +4,7 @@ in vec3 a_position;
 in vec3 a_color;
 in float a_alpha;
 
+out vec3 v_position;
 out vec3 v_color;
 out float v_alpha;
 
@@ -13,6 +14,7 @@ uniform mat4 model_view_matrix;
 void main()
 {
     gl_Position = proj_matrix * model_view_matrix * vec4(a_position, 1.0);
+    v_position = a_position;
     v_color = a_color;
     v_alpha = a_alpha;
 }
