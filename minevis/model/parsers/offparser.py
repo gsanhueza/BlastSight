acfd5ccd252e466e02b563b7ddebc9c39e104059
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-09-10 11:13:47 -0300

    Workaround works for linux, but not for windows, testing another monkeypatch

--- minevis/model/parsers/offparser.py
+++ minevis/model/parsers/offparser.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import multiprocessing
 import pandas as pd
 
 from qtpy.QtCore import QFileInfo
@@ -9,15 +8,8 @@ from .parser import Parser
 
 
 class OFFParser(Parser):
-    """
-    Pandas' current version (0.25) has an ugly memory leak when reading files (tested empirically).
-    Since MineVis is not meant to improve Pandas, we'll implement a workaround from
-    https://stackoverflow.com/a/39101287 (multiprocessing) with some bits from
-    https://stackoverflow.com/a/46322731 (pool.join(), pool.close())
-
-    """
     @staticmethod
-    def _load_file_mp(path: str) -> ParserData:
+    def load_file(path: str) -> ParserData:
         assert path.lower().endswith('off')
 
         with open(path, 'r') as fp:
@@ -41,13 +33,3 @@ class OFFParser(Parser):
             data.properties = properties
 
             return data
-
-    @staticmethod
-    def load_file(path: str) -> ParserData:
-        pool = multiprocessing.Pool(1)
-        result = pool.map(OFFParser._load_file_mp, [path])[0]
-        pool.close()
-        pool.join()
-        del pool
-
-        return result
--- minevis/model/parsers/parser.py
+++ minevis/model/parsers/parser.py
@@ -2,6 +2,37 @@
 
 from .parserdata import ParserData
 
+"""
+Pandas' current version (0.25) has an ugly memory leak when reading files (tested empirically).
+Since MineVis is not meant to improve Pandas, we'll implement a workaround from
+https://github.com/pandas-dev/pandas/issues/2659#issuecomment-415177442
+
+"""
+import sys
+import pandas as pd
+from ctypes import cdll, CDLL
+try:
+    cdll.LoadLibrary("libc.so.6")
+    libc = CDLL("libc.so.6")
+    libc.malloc_trim(0)
+except (OSError, AttributeError):
+    libc = None
+
+__old_del = getattr(pd.DataFrame, '__del__', None)
+
+
+def __new_del(self):
+    if __old_del:
+        __old_del(self)
+    libc.malloc_trim(0)
+
+
+if libc:
+    print('Applying monkeypatch for pd.DataFrame.__del__', file=sys.stderr)
+    pd.DataFrame.__del__ = __new_del
+else:
+    print('Skipping monkeypatch for pd.DataFrame.__del__: libc or malloc_trim() not found', file=sys.stderr)
+
 
 class Parser:
     @staticmethod
