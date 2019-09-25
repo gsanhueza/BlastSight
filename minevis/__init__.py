371a4144487962b6c617fe0e4ff29e328f9c9586
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-09-25 16:04:26 -0300

    Pandas' memory-leak workaround moved to __init__.py

---
+++ minevis/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+Pandas' current version (0.25) has an ugly memory leak when reading files (tested empirically).
+Since MineVis is not meant to improve Pandas, we'll implement a workaround from
+https://github.com/pandas-dev/pandas/issues/2659#issuecomment-415177442
+
+"""
+
+import pandas as pd
+from ctypes import cdll, CDLL
+
+try:
+    cdll.LoadLibrary('libc.so.6')
+    libc = CDLL('libc.so.6')
+    libc.malloc_trim(0)
+except (OSError, AttributeError):
+    libc = None
+
+__old_del = getattr(pd.DataFrame, '__del__', None)
+
+
+def __new_del(self):
+    if __old_del:
+        __old_del(self)
+    libc.malloc_trim(0)
+
+
+if libc:
+    pd.DataFrame.__del__ = __new_del
--- minevis/model/parsers/parser.py
+++ minevis/model/parsers/parser.py
@@ -1,33 +1,6 @@
 #!/usr/bin/env python
 
-import pandas as pd
-from ctypes import cdll, CDLL
 from .parserdata import ParserData
-
-"""
-Pandas' current version (0.25) has an ugly memory leak when reading files (tested empirically).
-Since MineVis is not meant to improve Pandas, we'll implement a workaround from
-https://github.com/pandas-dev/pandas/issues/2659#issuecomment-415177442
-
-"""
-try:
-    cdll.LoadLibrary('libc.so.6')
-    libc = CDLL('libc.so.6')
-    libc.malloc_trim(0)
-except (OSError, AttributeError):
-    libc = None
-
-__old_del = getattr(pd.DataFrame, '__del__', None)
-
-
-def __new_del(self):
-    if __old_del:
-        __old_del(self)
-    libc.malloc_trim(0)
-
-
-if libc:
-    pd.DataFrame.__del__ = __new_del
 
 
 class Parser:
