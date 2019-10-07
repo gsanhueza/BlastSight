ad3a8f53f268690a6a7fa225a8dc3465a67e8b59
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-10-07 12:24:40 -0300

    Dynamic slice example updated to comply with new sliced signals

--- examples/dynamicslice.py
+++ examples/dynamicslice.py
@@ -17,13 +17,60 @@ mesh = v.mesh_by_path(mesh_path, color=[0.0, 0.8, 0.6], alpha=0.6)
 blocks = v.blocks_by_path(blocks_path)
 points = v.points_by_path(blocks_path)
 
+
+"""
+These two methods are taken and adapted from the application.
+
+We offer the signals instead of automatically adding the slices,
+so you can decide what to do with them before committing them
+to the viewer.
+"""
+
+
+def slot_mesh_sliced(slice_dict: dict):
+    slice_list = slice_dict.get('slices', [])
+
+    for sliced_meshes in slice_list:
+        slices = sliced_meshes.get('sliced_vertices')
+        origin_id = sliced_meshes.get('origin_id')
+        _mesh = v.get_drawable(origin_id)
+
+        for i, vert_slice in enumerate(slices):
+            v.lines(vertices=vert_slice,
+                    color=_mesh.color,
+                    loop=True)
+
+
+def slot_blocks_sliced(slice_dict: dict):
+    slice_list = slice_dict.get('slices', [])
+
+    for sliced_blocks in slice_list:
+        slices = sliced_blocks.get('sliced_vertices')
+        values = sliced_blocks.get('sliced_values')
+        origin_id = sliced_blocks.get('origin_id')
+        _blocks = v.get_drawable(origin_id)
+
+        v.blocks(vertices=slices,
+                 values=values,
+                 vmin=_blocks.vmin,
+                 vmax=_blocks.vmax,
+                 colormap=_blocks.colormap,
+                 block_size=_blocks.block_size,
+                 alpha=1.0,
+                 )
+
+    # Auto-close viewer
+    v.close()
+
+
 """
 Slice mode means that you'll click two points in the screen,
 so BlastSight will automatically generate a plane that
 will pass through every visible mesh and block.
 """
 v.set_slice_mode()
-v.signal_file_modified.connect(v.close)
+v.signal_mesh_sliced.connect(slot_mesh_sliced)
+v.signal_blocks_sliced.connect(slot_blocks_sliced)
 
 v.fit_to_screen()
 v.show()
