de0db97c08e7705e1eb619314d3108d4dfb5a12f
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-10-10 17:52:46 -0300

    Improved signals example

--- examples/signals.py
+++ examples/signals.py
@@ -13,7 +13,18 @@ v.mesh_by_path('../test_files/caseron.off')
 
 
 def update_info(x: list):
-    v.setWindowTitle(f'{title} - {[attr.get("name") for attr in x]}')
+    update_title = []
+    for d in v.drawable_collection.values():
+        d.is_highlighted = False
+
+    for attr in x:
+        _id = attr.get('id')
+        name = attr.get('name')
+
+        v.get_drawable(_id).is_highlighted = True
+        update_title.append(name)
+
+    v.setWindowTitle(f'{title} - {update_title}')
     print(f'Attributes list: {x}')
 
 
