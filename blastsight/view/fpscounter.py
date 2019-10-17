b8bf27c0755455a6050bb9c4e38f8fec47d6f919
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2019-10-17 19:33:42 -0300

    Bugfix in FPSCounter, resolution settable in constructor

--- blastsight/view/fpscounter.py
+++ blastsight/view/fpscounter.py
@@ -4,15 +4,15 @@ import time
 
 
 class FPSCounter:
-    def __init__(self):
+    def __init__(self, resolution: float = 1.0):
         self.start_time = time.time()
-        self.resolution = 1.0
+        self.resolution = resolution
         self.counter = 0
 
     def tick(self, callback=lambda *args: None):
         self.counter += 1
         if time.time() - self.start_time > self.resolution:
             diff = (time.time() - self.start_time)
-            callback(self.counter * self.resolution / diff if diff else 0.0)
+            callback(self.counter / diff if diff else 0.0)
             self.counter = 0
             self.start_time = time.time()
