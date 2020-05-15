957a69f7673c40795c90d3e492dfa689c3237a77
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2020-05-15 09:31:55 -0400

    Updated __init__.py comment to avoid confusion in documentation from PyCharm

--- blastsight/__init__.py
+++ blastsight/__init__.py
@@ -5,12 +5,8 @@
 #  Distributed under the MIT License.
 #  See LICENSE for more info.
 
-"""
-Pandas' current version (0.25) has an ugly memory leak when reading files (tested empirically).
-Since BlastSight is not meant to improve Pandas, we'll implement a workaround from
-https://github.com/pandas-dev/pandas/issues/2659#issuecomment-415177442
-
-"""
+# Solving memory leak problem in pandas
+# https://github.com/pandas-dev/pandas/issues/2659#issuecomment-415177442
 
 import pandas as pd
 from ctypes import cdll, CDLL
