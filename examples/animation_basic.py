eecd4bae618f506f1900e24eeed1e9c749cdf68d
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2020-06-08 18:28:51 -0400

    Basic animation example added

---
+++ examples/animation_basic.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from blastsight.view.viewer import Viewer
+
+"""
+In this demo, we'll show how you can create a basic animation.
+
+An animation is interpreted as changing the state of the viewer one frame at the time.
+That means we'll define a function that makes a change in one single frame.
+The function must receive a single argument, of the same type of the 'start' and 'end' values.
+"""
+
+v = Viewer()
+mesh = v.load_mesh('../test_files/caseron.off', highlight=True)
+
+
+def autorotate(angle):
+    v.set_rotation_angle([0.0, -angle, 0.0])
+
+
+"""
+The animate() method receives a 'start' value, an 'end' value, a 'method' (the function that changes
+one frame in the viewer), and two optional kwargs: 'milliseconds' (how much time should the
+animation last) and 'steps' (smoothness of the animation depends on this).
+"""
+
+# Start animation
+v.animate(0, 360, autorotate, milliseconds=3000, steps=100)
+
+# Show viewer
+v.show()
