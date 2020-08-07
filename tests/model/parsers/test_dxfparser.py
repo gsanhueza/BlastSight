88f2de98d80deb041686754236b6d56d760b6502
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2020-08-07 10:22:03 -0400

    Test for lines in DXFParser added

--- tests/model/parsers/test_dxfparser.py
+++ tests/model/parsers/test_dxfparser.py
@@ -6,12 +6,17 @@ from tests.globals import *
 
 
 class TestDXFParser:
-    def test_load_file(self):
-        info = Parser.load_file(f'{TEST_FILES_FOLDER_PATH}/caseron.dxf')
+    def test_load_mesh(self):
+        info = Parser.load_file(f'{TEST_FILES_FOLDER_PATH}/caseron.dxf', hint='mesh')
         vertices = info.get('data').get('vertices')
         indices = info.get('data').get('indices')
         assert len(vertices) == 12
         assert len(indices) == 20
+
+    def test_load_lines(self):
+        info = Parser.load_file(f'{TEST_FILES_FOLDER_PATH}/polyline3d.dxf', hint='line')
+        vertices = info.get('data').get('vertices')
+        assert len(vertices) == 9
 
     def test_load_inexistent(self):
         with pytest.raises(Exception):
@@ -24,3 +29,7 @@ class TestDXFParser:
     def test_load_damaged(self):
         with pytest.raises(Exception):
             Parser.load_file(f'{TEST_FILES_FOLDER_PATH}/bad.dxf')
+
+    def test_save_file(self):
+        with pytest.raises(Exception):
+            Parser.save_file(None)
