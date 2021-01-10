4ce404cee695b0ca4ab34645678aaeaa081b1394
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2021-01-10 15:32:13 -0300

    Adding lines in text example

--- examples/render_text.py
+++ examples/render_text.py
@@ -20,10 +20,11 @@ def demo_text():
     # viewer.register_drawable(TextGL(text="Bonjour", position=[100.0, 0.0, 0.0], name='Bonjour', id=30))
 
     path = f'{pathlib.Path(__file__).parent.parent}/test_files/caseron.off'
-    mesh = viewer.load_mesh(path, alpha=0.1)
+    mesh = viewer.load_mesh(path, color=[1.0, 1.0, 0.0], alpha=0.1)
     min_bound, max_bound = mesh.bounding_box
     viewer.register_drawable(TextGL(text=f".y = {str(round(min_bound[0], 3))}", position=min_bound, id=35))
     viewer.register_drawable(TextGL(text=f".y = {str(round(max_bound[1], 3))}", position=[min_bound[0], max_bound[0], min_bound[2]], id=36))
+    viewer.lines(vertices=[min_bound, [min_bound[0], max_bound[0], min_bound[2]]], color=[1.0, 0.0, 0.0], thickness=20)
 
     viewer.show()
 
