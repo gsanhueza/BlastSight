3744e925cf4dd5c34a3f6d7409a529f861d9e387
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2021-01-23 15:14:12 -0300

    Improved transparency of text drawables

--- blastsight/view/glprograms/textprogram.py
+++ blastsight/view/glprograms/textprogram.py
@@ -5,6 +5,7 @@
 #  Distributed under the MIT License.
 #  See LICENSE for more info.
 
+from OpenGL.GL import *
 from .shaderprogram import ShaderProgram
 
 
@@ -12,3 +13,15 @@ class TextProgram(ShaderProgram):
     def __init__(self):
         super().__init__()
         self.base_name = 'Text'
+
+    def draw(self) -> None:
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+        glDepthMask(GL_FALSE)
+        glEnable(GL_CULL_FACE)
+
+        for gl_cull in [GL_FRONT, GL_BACK]:
+            glCullFace(gl_cull)
+            super().draw()
+
+        glDisable(GL_CULL_FACE)
+        glDepthMask(GL_TRUE)
