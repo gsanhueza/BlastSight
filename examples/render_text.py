2f6557ecf2c3c45cdd71c671b6a3d786176a70e9
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2021-04-11 11:37:14 -0400

    Render text example file now shows relevant mesh information when simulating being part of the grid

--- examples/render_text.py
+++ examples/render_text.py
@@ -17,24 +17,31 @@ def demo_text():
     path = f'{pathlib.Path(__file__).parent.parent}/test_files/caseron.off'
     mesh = viewer.load_mesh(path, color=[1.0, 1.0, 0.0], alpha=0.1)
     min_bound, max_bound = mesh.bounding_box
+    lengths = max_bound - min_bound
 
     # Show a grid using the bounding box of the figure
-    viewer.grid(origin=min_bound, lengths=max_bound - min_bound, color=[1.0, 0.8, 0.0])
+    grid = viewer.grid(origin=min_bound, lengths=lengths, color=[1.0, 0.8, 0.0], mark_separation=10)
 
-    # Get some positions
-    pos_x = [max_bound[0], min_bound[1], min_bound[2]]
-    pos_y = [min_bound[0], max_bound[1], min_bound[2]]
-    pos_z = [min_bound[0], min_bound[1], max_bound[2]]
+    # Labels in X
+    # Remember to separate the number text from the grid
+    for x in range(int(lengths[0] / grid.mark_separation) + 1):
+        pos = min_bound[0] + x * grid.mark_separation
+        viewer.text(text=f'{round(pos, 1)}', position=[pos, min_bound[1] - 2.0, min_bound[2]], scale=0.05)
 
-    # Setup our text
-    viewer.text(text=f'{pos_x}', position=pos_x)
-    viewer.text(text=f'{pos_y}', position=pos_y)
-    viewer.text(text=f'{pos_z}', position=pos_z)
+    # Labels in Y
+    for y in range(int(lengths[1] / grid.mark_separation) + 1):
+        pos = min_bound[1] + y * grid.mark_separation
+        viewer.text(text=f'{round(pos, 1)}', position=[min_bound[0] - 8.0, pos, min_bound[2] - 2.0], scale=0.05)
+
+    # Labels in Z
+    for z in range(int(lengths[2] / grid.mark_separation) + 1):
+        pos = min_bound[2] + z * grid.mark_separation
+        viewer.text(text=f'{round(pos, 1)}', position=[min_bound[0] - 8.0, min_bound[1], pos], scale=0.05)
 
     # Showcase orientations
-    viewer.text(text='Facing: Elevation', position=[0.0, 0.0, 0.0], orientation='elevation')
-    viewer.text(text='Facing: North', position=[0.0, 0.0, 3.0], orientation='north')
-    viewer.text(text='Facing: East', position=[0.0, 5.0, 6.0], orientation='east')
+    viewer.text(text='Facing: Elevation', position=mesh.center + [0.0, 0.0, 0.0], orientation='elevation')
+    viewer.text(text='Facing: North', position=mesh.center + [0.0, 0.0, 3.0], orientation='north')
+    viewer.text(text='Facing: East', position=mesh.center + [0.0, 5.0, 6.0], orientation='east')
 
     viewer.show()
 
