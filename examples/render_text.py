3b64eaeaa53f2c654e396eb7ffecdc7fbac8df49
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2021-05-15 19:07:15 -0400

    Updated render_text example to use the grid's new API

--- examples/render_text.py
+++ examples/render_text.py
@@ -16,26 +16,32 @@ def demo_text():
     # Load a mesh
     path = f'{pathlib.Path(__file__).parent.parent}/test_files/caseron.off'
     mesh = viewer.load_mesh(path, color=[1.0, 1.0, 0.0], alpha=0.1)
-    min_bound, max_bound = mesh.bounding_box
-    lengths = max_bound - min_bound
 
-    # Show a grid using the bounding box of the figure
-    grid = viewer.grid(origin=min_bound, lengths=lengths, color=[1.0, 0.8, 0.0], mark_separation=10)
+    # Setup the grid using the bounding box of the figure
+    min_bound, max_bound = mesh.bounding_box
+    size = max_bound - min_bound
+    mark_separation = 10
+
+    viewer.grid.origin = min_bound
+    viewer.grid.size = max_bound - min_bound
+    viewer.grid.color = [1.0, 0.8, 0.0]
+    viewer.grid.mark_separation = mark_separation
+    viewer.grid.is_visible = True
 
     # Labels in X
     # Remember to separate the number text from the grid
-    for x in range(int(lengths[0] / grid.mark_separation) + 1):
-        pos = min_bound[0] + x * grid.mark_separation
+    for x in range(int(size[0] / mark_separation) + 1):
+        pos = min_bound[0] + x * mark_separation
         viewer.text(text=f'{round(pos, 1)}', position=[pos, min_bound[1] - 2.0, min_bound[2]], scale=0.05)
 
     # Labels in Y
-    for y in range(int(lengths[1] / grid.mark_separation) + 1):
-        pos = min_bound[1] + y * grid.mark_separation
+    for y in range(int(size[1] / mark_separation) + 1):
+        pos = min_bound[1] + y * mark_separation
         viewer.text(text=f'{round(pos, 1)}', position=[min_bound[0] - 8.0, pos, min_bound[2] - 2.0], scale=0.05)
 
     # Labels in Z
-    for z in range(int(lengths[2] / grid.mark_separation) + 1):
-        pos = min_bound[2] + z * grid.mark_separation
+    for z in range(int(size[2] / mark_separation) + 1):
+        pos = min_bound[2] + z * mark_separation
         viewer.text(text=f'{round(pos, 1)}', position=[min_bound[0] - 8.0, min_bound[1], pos], scale=0.05)
 
     # Showcase orientations
