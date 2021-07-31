103131bfb11b2499a19715555e49ffab23c89dbc
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2021-07-31 13:52:34 -0400

    Improved OFF parser, now it can load polygons-per-index, instead of only triangles-per-index

--- blastsight/model/parsers/offparser.py
+++ blastsight/model/parsers/offparser.py
@@ -17,14 +17,41 @@ class OFFParser(Parser):
     def load_file(path: str, *args, **kwargs) -> dict:
         assert path.lower().endswith('off')
 
+        # Process header
         with open(path, 'r') as fp:
             assert 'OFF' == fp.readline().strip()
             n_vertices, n_faces, n_edges = map(int, fp.readline().strip().split(' '))
 
-            vertices = pd.read_csv(path, skiprows=1, nrows=n_vertices,
-                                   usecols=range(3), sep=' ').to_numpy(dtype=float)
-            indices = pd.read_csv(path, skiprows=1 + n_vertices, nrows=n_faces, delimiter=' ',
-                                  usecols=range(1, 4)).to_numpy(dtype=int)
+        # Process vertices
+        vertices = pd.read_csv(path, skiprows=1, nrows=n_vertices, usecols=range(3), sep=' ').to_numpy(dtype=float)
+
+        # Pre-process indices "columns" (Simplify loading OFF files with variable column numbers in indices)
+        with open(path, 'r') as temp_f:
+            col_count = [len(line.split(' ')) for line in temp_f.readlines()]
+
+        # Process indices
+        indices = []
+        raw_indices = pd.read_csv(path, skiprows=n_vertices + 2, nrows=n_faces, delimiter=' ',
+                                  header=None, names=list(map(str, range(max(col_count)))))
+
+        # Accept indices directly if they all describe only triangles
+        if raw_indices.iloc[:, 0].max() == 3:
+            indices = raw_indices[raw_indices.columns[-3:]].to_numpy(dtype=int)
+
+        # Triangulate polygons otherwise
+        else:
+            for _, idx in raw_indices.iterrows():
+                counter = idx[0]
+                row_indices = idx.to_numpy(dtype=int)[1:]
+                fan_starter = row_indices[0]
+                fan_indices = row_indices[1:]
+
+                while counter > 2:
+                    indices += [fan_starter, fan_indices[0], fan_indices[1]]
+                    fan_indices = fan_indices[1:]
+                    counter -= 1
+
+            indices = np.array(indices, np.int32).reshape((-1, 3))
 
         # Data
         data = {
