cdf5e6a602a3d66823aa5cd0847c24620c685f28
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2021-08-21 12:15:01 -0400

    Fallback font added when user does not have expected font file

--- blastsight/view/glprograms/textprogram.py
+++ blastsight/view/glprograms/textprogram.py
@@ -6,6 +6,7 @@
 #  See LICENSE for more info.
 
 import freetype
+import pathlib
 
 from OpenGL.GL import *
 from .shaderprogram import ShaderProgram
@@ -31,13 +32,17 @@ class TextProgram(ShaderProgram):
     # Static attributes
     characters = {}
 
+    # Select font
     try:
         fontfile = r'/usr/share/fonts/TTF/cour.ttf'
         face = freetype.Face(fontfile)
     except freetype.ft_errors.FT_Exception:
-        fontfile = r'C:\Windows\Fonts\cour.ttf'
-        face = freetype.Face(fontfile)
-
+        try:
+            fontfile = r'C:\Windows\Fonts\cour.ttf'
+            face = freetype.Face(fontfile)
+        except freetype.ft_errors.FT_Exception:
+            fontfile = f'{pathlib.Path(__file__).parent.parent}/gui/UI/fonts/NotoSans-Medium.ttf'
+            face = freetype.Face(fontfile)
 
     # Set character size
     face.set_char_size(48 * 64)
