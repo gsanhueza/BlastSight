fa1791f8e21d63c19045d6f5cd190d96b85ec0a5
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2022-03-13 14:32:56 -0300

    Release 0.9.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='blastsight',
-    version='0.9.4',
+    version='0.9.5',
     author='Gabriel Sanhueza',
     description='A 3D visualization library oriented to mining applications',
     long_description=long_description,
